e4984c40a930a36965907bedad6bc0a6d8c7c701
DianaGakuya <deegakuya@gmail.com>
2024-01-28 20:39:24 +0300

    Using the app's context to perform database operations

--- server/seeds.py
+++ server/seeds.py
@@ -1,2 +1,5 @@
 from app import app  # Import the Flask app object
 from models import db, GameRecord  # Import the database object and GameRecord model from models
+
+# Using the app's context to perform database operations
+with app.app_context():
