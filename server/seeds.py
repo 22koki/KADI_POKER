f6ef886d4283cbbefae6a9133be0fd48e29a7901
DianaGakuya <deegakuya@gmail.com>
2024-01-28 20:27:12 +0300

    Define columns for the User model

--- server/seeds.py
+++ server/seeds.py
@@ -7,3 +7,11 @@ db = SQLAlchemy()
 class User(db.Model, SerializerMixin):
     # Define a table name for the User model
     __tablename__ = 'users'
+
+     # Define columns for the User model
+    user_id = db.Column(db.Integer, primary_key=True)  # User's unique ID
+    username = db.Column(db.String(50), unique=True, nullable=False)  # Username, must be unique
+    password_hash = db.Column(db.String(128), nullable=False)  # Hash of the user's password
+    created_at = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)  # Timestamp of user creation
+
+    
