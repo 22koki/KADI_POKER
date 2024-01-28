79a46ad583afa43645096d27f1eb679904552615
Cherono-mercy <cheronom142@gmail.com>
2024-01-28 23:48:22 +0800

    Created the home route

---
+++ server/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User, GameRecord
+from game_logic import PokerGame
+from flask import abort
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})  # Adjust the origin as needed
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///poker_records.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+migrate = Migrate(app, db)
+db.init_app(app)
+
+# Create tables based on the defined models
+with app.app_context():
+    db.create_all()
+
+poker_game = PokerGame()
+
+# Index Route
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "Welcome to the Poker API!"})
