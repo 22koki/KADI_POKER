ace26a42aa0bb612ba94e86f681ad1fff7a3835d
mgkEdits <elijahnyasiando.mn@gmail.com>
2024-01-28 23:47:01 +0300

    navCenter html component

--- client/src/components/game/navCenter.js
+++ client/src/components/game/navCenter.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function navCenter() {
   return (
-    <div>navCenter</div>
+    <div className='navCenter-crd'>
+        <div className='left-navBar'>
+        <a href=''>Timer : {minutes}m {seconds}s</a>
+        </div>
+
+        <div className='right-navBar'>
+        <h2>Running Score :</h2>
+            
+        </div>
+    </div>
   )
 }
 
