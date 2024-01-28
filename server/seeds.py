9eb63321c2aeebb2a2479d4cfff0978d57d5c358
DianaGakuya <deegakuya@gmail.com>
2024-01-28 20:41:15 +0300

    Define a list of GameRecord objects to be added to the database

--- server/seeds.py
+++ server/seeds.py
@@ -6,4 +6,19 @@ with app.app_context():
 
 #Delete all existing records in the GameRecord table
     GameRecord.query.delete()
+
+     # Define a list of GameRecord objects to be added to the database
+    game_records = [
+        GameRecord(game_id="1", user_id="1", result="0"),
+        GameRecord(game_id="2", user_id="1", result="1"),
+        GameRecord(game_id="3", user_id="2", result="0"),
+        GameRecord(game_id="4", user_id="3", result="1"),
+        GameRecord(game_id="5", user_id="3", result="1"),
+        GameRecord(game_id="6", user_id="3", result="0"),
+        GameRecord(game_id="7", user_id="4", result="0"),
+        GameRecord(game_id="8", user_id="4", result="1"),
+        GameRecord(game_id="9", user_id="6", result="1"),
+        GameRecord(game_id="10", user_id="5", result="1"),
+        GameRecord(game_id="11", user_id="5", result="0"),
+    ]
  
