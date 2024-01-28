a5dbf5a603c79eb641849abb8b5ad608c2c7d832
DianaGakuya <deegakuya@gmail.com>
2024-01-28 20:26:32 +0300

    Define a table name for the User model

--- server/seeds.py
+++ server/seeds.py
@@ -3,3 +3,7 @@ from sqlalchemy_serializer import SerializerMixin
 
 # Initialize SQLAlchemy - this is the core part of the ORM that will be used for database interactions.
 db = SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
+    # Define a table name for the User model
+    __tablename__ = 'users'
