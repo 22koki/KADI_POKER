7bac87cb4937aa86221101d5bff0789e5123f65c
Matthew <+>
2024-01-29 00:59:58 +0300

    import recoilstate and recordvalue

---
+++ client/src/components/Viewscores.js
@@ -0,0 +1,3 @@
+import React, { useEffect, useState } from 'react';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { gameScoresState } from './atoms'; // Adjust the path as needed
