9c6860cecd2c90368c8f168c3c8d48241df09efb
fay <wahomeimani@gmail.com>
2024-01-29 09:39:21 +0300

    updated

--- client/src/components/game/navCenter.js
+++ client/src/components/game/navCenter.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import Buttons from './Buttons';
+import React from 'react'
+import { useState, useEffect } from 'react';
 
-const WelcomeMessage = () => {
+function NavCenter() {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000);
+
+    return () => clearInterval(timerInterval);
+  }, []);
+
+  // Convert seconds to minutes and seconds
+  const minutes = Math.floor(timer / 60);
+  const seconds = timer % 60;
+
   return (
-    <div className='landing-sqr'>
-      <h1>Welcome to Poker Land</h1>
-      <Buttons />
-    </div>
-  );
-};
+    <div className='navCenter-crd'>
+        <div className='left-navBar'>
+        <a href=''>Timer : {minutes}m {seconds}s</a>
+        </div>
 
-export default WelcomeMessage;
+        <div className='right-navBar'>
+        {/* <h2>Running Score :</h2> */}
+            
+        </div>
+    </div>
+  )
+}
+
+export default NavCenter
